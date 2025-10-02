2181fc99c3243fc4638ef4ea5f7b81c7ba405927
EstebanLinharesBarral <elinaresb24@fpcoruna.afundacion.org>
2025-10-02 10:34:11 +0200

    Ejercicio 4

---
+++ www/site3/carrito.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <?php
+        $productos = array("Manzana"=>1.50, "Pan" =>0.70, "Tomate" => 1.30);
+        //var_dump($productos);
+        echo "<table>";
+        echo "<tr><th>Productos</th><th>Precio</th></tr>";
+        foreach( $productos as $prod => $pre){
+            echo "<tr>";
+            echo "<td>" . $prod . "</td>";
+            echo "<td>" . $pre . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    ?>
+</body>
+</html>
